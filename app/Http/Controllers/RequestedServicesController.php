5b44e85ccccee6eb4287291f4f46d84a0c7b4a5a
MotanyaIsaack <isaackmotanya6@gmail.com>
2019-07-08 02:55:09 +0300

    Testing Complete Functionality

--- app/Http/Controllers/RequestedServicesController.php
+++ app/Http/Controllers/RequestedServicesController.php
@@ -152,7 +152,7 @@ class RequestedServicesController extends Controller
             ->where('userId', $userId)
             ->update($status);
         
-            // return response()->json($userId, 200);
+            return response()->json($userId, 200);
         if ($updateQuery) {
             return response()->json(['message' => "Completed"], 200);
         }else{
